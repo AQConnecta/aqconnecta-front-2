09313da696f7ab675a8c676ee6fa5063a46d6523
Kaio Felps <kaiofelipe2005@gmail.com>
2025-09-21 22:57:00 -0300

    feat: add button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,10 @@
+import { ButtonIcon } from "./icon";
+import { ButtonRoot } from "./root";
+
+export type TButtonSize = "sm" | "md";
+export type TButtonVariant = "primary" | "ghost-primary";
+
+export default {
+  Root: ButtonRoot,
+  Icon: ButtonIcon,
+};
---
+++ src/components/button/root.tsx
@@ -0,0 +1,56 @@
+import * as Slot from "@radix-ui/react-slot";
+import clsx from "clsx";
+import type { ButtonHTMLAttributes, PropsWithChildren } from "react";
+import type { TButtonSize, TButtonVariant } from ".";
+
+type ButtonCoreProps = PropsWithChildren<{
+  className?: string;
+  variant: TButtonVariant;
+  size: TButtonSize;
+}>;
+
+type ButtonProps =
+  | ({
+      asChild?: false;
+    } & ButtonCoreProps &
+      ButtonHTMLAttributes<HTMLButtonElement>)
+  | ({ asChild: true } & ButtonCoreProps);
+
+export function ButtonRoot({
+  children,
+  asChild,
+  className,
+  variant,
+  size,
+  ...props
+}: ButtonProps) {
+  const Element = asChild ? Slot.Root : "button";
+  return (
+    <Element
+      data-variant={variant}
+      data-size={size}
+      className={clsx(
+        "group flex gap-3 items-center justify-center font-bold",
+        "transition-all duration-100 cursor-default",
+        size === "md" && "px-8 py-3 rounded-xl",
+        size === "sm" && "px-5 py-2 rounded-xl",
+        variant === "primary" && [
+          "drop-shadow-[0_2px_0] drop-shadow-black/15 bg-primary-600 text-white",
+          "hover:bg-primary-700 active:opacity-90",
+        ],
+        variant === "ghost-primary" && [
+          // usar o before é necessário se quisermos evitar que o drop-shadow afete
+          // o conteúdo dentro do botão devido a translucidez do background
+          "before:absolute before:inset-0 relative before:rounded-xl",
+          "before:bg-primary-600/25 text-primary-600",
+          "hover:before:bg-primary-700/25 active:before:bg-primary-700/40",
+          "before:drop-shadow-[0_2px_0] before:drop-shadow-black/15",
+        ],
+        className && className,
+      )}
+      {...props}
+    >
+      {children}
+    </Element>
+  );
+}
