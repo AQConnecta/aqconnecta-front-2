0b4be4536014c93513c468230ed77a545b96bcc8
Kaio Felps <kaiofelipe2005@gmail.com>
2025-09-28 16:26:34 -0300

    change(Routes): nest auth routes in `auth` object

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -1,7 +1,9 @@
 export const Routes = Object.freeze({
   home: "/",
-  register: "/registre-se",
-  login: "/login",
+  auth: {
+    register: "/registre-se",
+    login: "/login",
+  },
   vacancies: "/vagas",
   userSubmits: "/minhas-candidaturas",
 });
