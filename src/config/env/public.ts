ca69716c8da678bece042ec2b951f1572d3937f4
Kaio Felps <kaiofelipe2005@gmail.com>
2025-09-28 16:27:08 -0300

    fix(PublicEnv): add server url as public env and fix public environment variables not being statically replaced by nextjs

--- src/config/env/public.ts
+++ src/config/env/public.ts
@@ -1,11 +1,25 @@
-import { getEnvVarOrThrow } from "./utils";
+import { MissingEnvVarError } from "@/core/errors/missing-env-var-error";
 
 /**
  * Essas variáveis são substituídas pelo valor literal durante o build. Esses
  * valores não podem ser alterados on-fly, e precisam ter o prefixo
  * `NEXT_PUBLIC_`.
+ *
+ * Essa constante não pode utilizar a função utilitária `getEnvVarOrThrow` para
+ * obter as variáveis de ambiente pois, como são substituídos no build, a buildtool
+ * não consegue descobrir dinâmicamente qual variável está sendo buscada no `process.env`.
  */
 export const PublicEnv = Object.freeze({
-  appUrl: getEnvVarOrThrow("NEXT_PUBLIC_APPLICATION_URL"),
-  appName: getEnvVarOrThrow("NEXT_PUBLIC_APPLICATION_NAME"),
+  appUrl: process.env.NEXT_PUBLIC_APPLICATION_URL!,
+  appName: process.env.NEXT_PUBLIC_APPLICATION_NAME!,
+  serverUrl: process.env.NEXT_PUBLIC_SERVER_URL!,
 });
+
+if (!PublicEnv.appUrl)
+  throw new MissingEnvVarError("NEXT_PUBLIC_APPLICATION_URL");
+
+if (!PublicEnv.appName)
+  throw new MissingEnvVarError("NEXT_PUBLIC_APPLICATION_NAME");
+
+if (!PublicEnv.serverUrl)
+  throw new MissingEnvVarError("NEXT_PUBLIC_SERVER_URL");
