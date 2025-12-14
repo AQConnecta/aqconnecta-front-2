678491371b59e762043a4e86426bb77459042411
Kaio Felps <kaiofelipe2005@gmail.com>
2025-12-14 03:30:12 -0300

    style: add margin bottom to header

--- src/app/(main)/(public)/registre-se/page.tsx
+++ src/app/(main)/(public)/registre-se/page.tsx
@@ -5,7 +5,7 @@ import { RegisterForm } from "./form";
 export default function Register() {
   return (
     <Main>
-      <Heading level={3} className="mb-6 mt-8">
+      <Heading level={3} className="mb-6">
         Registre-se
       </Heading>
       <RegisterForm />
