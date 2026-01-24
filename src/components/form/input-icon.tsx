f47b7cb2a62caabf7139019e2b0aa3a06baebfe2
Kaio Felps <kaiofelipe2005@gmail.com>
2026-01-24 19:09:06 -0300

    change(Form.InputIcon): expose icon props

--- src/components/form/input-icon.tsx
+++ src/components/form/input-icon.tsx
@@ -1,14 +1,24 @@
-import type { Icon } from "@phosphor-icons/react";
+import type { Icon, IconProps } from "@phosphor-icons/react";
+import clsx from "clsx";
 
 type Props = {
   icon: Icon;
-};
+} & IconProps;
 
-export function InputIcon({ icon: I }: Props) {
+export function InputIcon({
+  icon: I,
+  className,
+  weight = "fill",
+  ...props
+}: Props) {
   return (
     <I
-      weight="fill"
-      className="shrink-0 text-gray-600 small-width:size-5 max-small-width:size-4 my-2"
+      {...props}
+      weight={weight}
+      className={clsx(
+        "shrink-0 text-gray-600 small-width:size-5 max-small-width:size-4 my-2",
+        className,
+      )}
     />
   );
 }
