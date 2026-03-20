b155f65bbc4bdee9b41f5922354006278f97fab7
Kaio Felps <kaiofelipe2005@gmail.com>
2026-03-20 11:11:57 -0300

    change: move `HotCompetences` to feed layout

--- src/app/(main)/(private)/(feed)/layout.tsx
+++ src/app/(main)/(private)/(feed)/layout.tsx
@@ -1,11 +1,15 @@
 import type { ReactElement } from "react";
 import { SidebarAuthUserProfile } from "@/ui/sidebar-profile";
+import { HotCompetences } from "./hot-competences";
 
 type Props = { children: ReactElement };
 
 export default function FeedLayout({ children }: Props) {
   return (
     <div className="flex justify-center items-start gap-6 w-full-with-margins mx-auto">
+      <aside>
+        <HotCompetences />
+      </aside>
       {children}
       <aside className="flex flex-col gap-4 w-fit">
         <SidebarAuthUserProfile />
--- src/app/(main)/(private)/(feed)/page.tsx
+++ src/app/(main)/(private)/(feed)/page.tsx
@@ -1,14 +1,10 @@
 import { Main } from "@/components/main";
-import { HotCompetences } from "./hot-competences";
 import { Vacancies } from "./vacancies";
 
 export default function Home() {
   return (
-    <>
-      <HotCompetences />
-      <Main className="w-full! mx-0!">
-        <Vacancies />
-      </Main>
-    </>
+    <Main className="w-full! mx-0!">
+      <Vacancies />
+    </Main>
   );
 }
