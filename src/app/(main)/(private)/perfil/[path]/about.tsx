a5d10bdb30de01799a486d8a7194623c8484eb32
Kaio Felps <kaiofelipe2005@gmail.com>
2026-03-28 02:27:34 -0300

    feat(profile page): add form to edit biography (not working yet)

--- src/app/(main)/(private)/perfil/[path]/about.tsx
+++ src/app/(main)/(private)/perfil/[path]/about.tsx
@@ -1,6 +1,16 @@
+/**
+ * TODO: salvar as alterações no banco de dados
+ * - um texto vazio deve ser transformado pra nulo/undefined visando remover a biografia atual
+ */
+
+"use client";
+
 import { SparkleIcon } from "@phosphor-icons/react/dist/ssr/Sparkle";
-import type { ReactNode } from "react";
+import { type ReactNode, useId, useState } from "react";
 import { Alert } from "@/components/alert";
+import Button from "@/components/button";
+import Dialog from "@/components/dialog";
+import Form from "@/components/form";
 import type { UsuarioCompleto } from "@/core/types/usuario-completo";
 import { SectionContainer } from "./section-container";
 
@@ -27,6 +37,44 @@ export function About({ completeUser, userOwnsProfile }: Props) {
   );
 }
 
+function EditDialog({
+  open,
+  setOpen,
+}: {
+  setOpen: (open: boolean) => void;
+  open: boolean;
+}) {
+  const formId = useId();
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Container>
+        <Dialog.Header title="Editar biografia" className="capitalize" />
+        <Dialog.Description className="mb-3">
+          Edite a sua biografia para que os outros usuários possam te conhecer.
+        </Dialog.Description>
+
+        <form id={formId}>
+          <Form.Input label="Biografia" type="textarea" required></Form.Input>
+        </form>
+
+        <Dialog.ActionsContainer>
+          <Dialog.ActionsContainer.LeftArea>
+            <Dialog.Close asChild>
+              <Button.Root type="button" variant="outline">
+                Cancelar
+              </Button.Root>
+            </Dialog.Close>
+
+            <Button.Root type="submit" form={formId} color="primary">
+              Salvar
+            </Button.Root>
+          </Dialog.ActionsContainer.LeftArea>
+        </Dialog.ActionsContainer>
+      </Dialog.Container>
+    </Dialog.Root>
+  );
+}
+
 function Wrapper({
   children,
   shouldShowEditButton,
@@ -34,14 +82,20 @@ function Wrapper({
   children: ReactNode;
   shouldShowEditButton: boolean;
 }) {
+  const [editDialogIsOpen, setEditDialogIsOpen] = useState(false);
+
   return (
-    <SectionContainer
-      icon={SparkleIcon}
-      title="Sobre"
-      shouldShowEditButton={shouldShowEditButton}
-      editButtonLabel="Editar sua biografia"
-    >
-      {children}
-    </SectionContainer>
+    <>
+      <SectionContainer
+        icon={SparkleIcon}
+        title="Sobre"
+        shouldShowEditButton={shouldShowEditButton}
+        editButtonLabel="Editar sua biografia"
+        onEditButtonClick={() => setEditDialogIsOpen(true)}
+      >
+        {children}
+      </SectionContainer>
+      <EditDialog open={editDialogIsOpen} setOpen={setEditDialogIsOpen} />
+    </>
   );
 }
