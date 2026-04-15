9b1c2d4bf5ace0c89aba74bef9fe2872621daefb
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-15 10:02:10 -0300

    feat(pages/profile): add education entry create dialog

--- src/app/(main)/(private)/perfil/[path]/education.tsx
+++ src/app/(main)/(private)/perfil/[path]/education.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { ScrollArea } from "@base-ui/react";
 import { GraduationCapIcon } from "@phosphor-icons/react/dist/ssr/GraduationCap";
 import { Alert } from "@/components/alert";
+import Button from "@/components/button";
+import Dialog from "@/components/dialog";
+import Form from "@/components/form";
 import type { UsuarioCompleto } from "@/core/types/usuario-completo";
 import type { FormacaoAcademica } from "@/core/types/value-objects/formacao-academica";
 import { SectionContainer } from "./section-container";
+import { UniversitiesCombobox } from "./universities-combobox";
 
 type Props = {
   completeUser: UsuarioCompleto;
@@ -33,7 +40,7 @@ export function Education({ completeUser, userOwnsProfile }: Props) {
       icon={GraduationCapIcon}
       title="Formação Acadêmica"
       shouldShowEditButton={userOwnsProfile}
-      editButtonLabel="Editar sua formação acadêmica"
+      actionContent={<CreateDialog />}
     >
       {content}
     </SectionContainer>
@@ -52,12 +59,83 @@ function AcademicTraining({ item }: { item: FormacaoAcademica }) {
   return (
     <div className="not-last-of-type:pb-3 not-first-of-type:mt-3">
       <p className="mb-1">
-        <span className="font-medium">{item.universidade.nomeInstituicao}</span>{" "}
+        <span className="font-medium">{item.universidade.nomeInstituicao}</span>
         - {item.descricao}
       </p>
       <span className="font-light">
         {startDate} - {endDate}
       </span>
     </div>
+  );
+}
+
+function CreateDialog() {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <SectionContainer.AddButton addButtonLabel="Adicionar nova formação acadêmica" />
+      </Dialog.Trigger>
+      <Dialog.Container className="flex flex-col">
+        <Dialog.Header
+          title="Adicionar nova formação acadêmica"
+          className="capitalize"
+        />
+
+        <ScrollArea.Root className="relative flex-1 min-h-0 overflow-hidden flex flex-col ">
+          <ScrollArea.Viewport className="flex-1 min-h-0 overflow-y-auto p-1">
+            <ScrollArea.Content className="">
+              <form className="flex flex-col gap-3">
+                <UniversitiesCombobox />
+
+                <Form.Input
+                  type="text"
+                  label="Diploma"
+                  required
+                  placeholder="ex: Bacharelado em Ciência da Computação"
+                />
+
+                <Form.Input
+                  type="textarea"
+                  label="Descrição"
+                  className="p-0"
+                  required
+                  placeholder="Descreva o curso, grau de instrução, etc."
+                  asChild
+                  inputWrapperClassname="p-0"
+                >
+                  <textarea className="p-3 resize-y" />
+                </Form.Input>
+
+                <Form.Switch label="Formação atual" />
+
+                <div className="flex items-start gap-4 w-full max-sm:flex-col">
+                  <Form.DatePicker
+                    className="w-full"
+                    label="Data de início"
+                    required
+                  />
+                  <Form.DatePicker
+                    className="w-full"
+                    label="Data de formação"
+                    required
+                  />
+                </div>
+              </form>
+            </ScrollArea.Content>
+          </ScrollArea.Viewport>
+        </ScrollArea.Root>
+
+        <Dialog.ActionsContainer>
+          <Dialog.ActionsContainer.LeftArea>
+            <Dialog.Close asChild>
+              <Button.Root variant="outline">Cancelar</Button.Root>
+            </Dialog.Close>
+            <Button.Root variant="default" color="primary">
+              Adicionar
+            </Button.Root>
+          </Dialog.ActionsContainer.LeftArea>
+        </Dialog.ActionsContainer>
+      </Dialog.Container>
+    </Dialog.Root>
   );
 }
