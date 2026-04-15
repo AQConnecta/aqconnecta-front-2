28592f4960a2d758e5618b2add568cab3023d8bd
Kaio Felps <kaiofelipe2005@gmail.com>
2026-04-15 09:39:37 -0300

    change(`Dialog` component): wrap content with viewport to stop dialog from being taller than the viewport height

--- src/components/dialog/container.tsx
+++ src/components/dialog/container.tsx
@@ -10,17 +10,21 @@ export function DialogContainer({ children, className }: Props) {
   return (
     <DialogPrimitive.Portal>
       <DialogPrimitive.Backdrop className="fixed inset-0 bg-black/40" />
-      <DialogPrimitive.Popup
-        className={clsx(
-          "fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 card transition-all duration-150",
-          "data-ending-style:opacity-0 data-ending-style:scale-90",
-          "data-starting-style:opacity-0 data-starting-style:scale-90",
-          "w-full-with-margins max-w-2xl",
-          className && className,
-        )}
+      <DialogPrimitive.Viewport
+        className={clsx("fixed inset-0 flex justify-center items-center p-6")}
       >
-        {children}
-      </DialogPrimitive.Popup>
+        <DialogPrimitive.Popup
+          className={clsx(
+            "relative card transition-all duration-150",
+            "data-ending-style:opacity-0 data-ending-style:scale-90",
+            "data-starting-style:opacity-0 data-starting-style:scale-90",
+            "w-full-with-margins max-w-2xl max-h-full min-h-0",
+            className && className,
+          )}
+        >
+          {children}
+        </DialogPrimitive.Popup>
+      </DialogPrimitive.Viewport>
     </DialogPrimitive.Portal>
   );
 }
