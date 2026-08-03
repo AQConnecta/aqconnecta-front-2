6f3625cbb42edd761eddb6229460bc14ec245fa5
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-03 00:46:50 -0300

    change(pages/profile/academic training): displays the education's diploma properly

--- src/app/(main)/(private)/perfil/[path]/academic-training/index.tsx
+++ src/app/(main)/(private)/perfil/[path]/academic-training/index.tsx
@@ -82,9 +82,10 @@ function AcademicTraining({
   return (
     <div className="not-last-of-type:pb-3 not-first-of-type:mt-3">
       <p className="mb-1">
-        <span className="font-medium">{item.universidade.nomeInstituicao}</span>{" "}
-        - {item.descricao}
+        <span className="font-medium">{item.universidade.nomeInstituicao}</span>
+        {item.diploma && ` - ${item.diploma}`}
       </p>
+      <p className="text-balance whitespace-pre-wrap">{item.descricao}</p>
       <span className="font-light">
         {startDate} - {endDate}
       </span>
